a0ae0b51b99c38e73bbea98c78670119ed09cd62
basha97 <bashaadil2727@gmail.com>
2019-02-05 18:14:16 +0530

    Controller Updates

---
+++ backend/resources/views/Email/passwordReset.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Change your password Request
+
+Click on the button  below to change Password
+
+@component('mail::button', ['url' => 'http://localhost:4200/response-password-reset?token='.'UFWaWDBIfMqPokeEL1WTNQLpYIPbf769evl3BbFc5PyCLaMu2l'])
+Reset Password
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
