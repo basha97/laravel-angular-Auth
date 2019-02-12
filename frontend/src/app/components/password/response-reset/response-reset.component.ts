056a0d7e874299163085d84f4c842ab9ce61b0cc
basha97 <bashaadil2727@gmail.com>
2019-02-12 19:27:07 +0530

    Last Process

--- frontend/src/app/components/password/response-reset/response-reset.component.ts
+++ frontend/src/app/components/password/response-reset/response-reset.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { JarwisService } from 'src/app/services/jarwis.service';
 
 @Component({
   selector: 'app-response-reset',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./response-reset.component.scss']
 })
 export class ResponseResetComponent implements OnInit {
+	public error = [];
 
-  constructor() { }
+	public form = {
+		email: null,
+		password: null,
+		password_confirmation: null,
+		resetToken: null
+	};
 
-  ngOnInit() {
-  }
+  	constructor(
+		  	private route: ActivatedRoute,
+			private jarwish: JarwisService
+		  ) { 
+		  route.queryParams.subscribe(params => {
+			this.form.resetToken = params['token'];
+		  });
+	  }
+
+  	ngOnInit() {
+	  }
+	  
+	onSubmit(){
+		this.jarwish.changePassword(this.form).subscribe(
+			data => this.handleResponse(data),
+			error => this.handleError(error)
+		);
+	}
+
+	handleResponse(data){
+
+	}
+
+	handleError(error){
+
+	}
 
 }
